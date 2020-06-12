513f61d32ec782baf4bea3d564f89595f66f921b
ben <gibumkang@gmail.com>
2020-06-12 06:48:20 -0700

    Update proposal section with link

--- proposal-banner.php
+++ proposal-banner.php
@@ -3,5 +3,5 @@
     <p>
         Find out how much your property will rent for and get a free management proposal.
     </p>
-    <a href="#">Free Proposal</a>
+    <a href="/free-proposal/">Free Proposal</a>
 </div>
